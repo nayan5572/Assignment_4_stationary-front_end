72df8d7c9b0ca796d12f4d5fa971e19861387ac5
nayan5572 <nayan.dev572@gmail.com>
2025-02-04 03:22:38 +0600

    vercel

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -11,8 +11,8 @@ import { logOut, setUser } from "../feathers/auth/authSlice";
 import { toast } from "sonner";
 
 const baseQuery = fetchBaseQuery({
-  // baseUrl: "https://stationery-shop-server-main.vercel.app/api",
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: "https://stationery-shop-server-main.vercel.app/api",
+  // baseUrl: "http://localhost:5000/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
@@ -38,10 +38,13 @@ const baseQueryRefreshToken: BaseQueryFn<
   }
 
   if (result?.error?.status === 401) {
-    const res = await fetch("http://localhost:5000/api/auth/refresh-token", {
-      method: "POST",
-      credentials: "include",
-    });
+    const res = await fetch(
+      "https://stationery-shop-server-main.vercel.app/api/auth/refresh-token",
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
     const data = await res.json();
     if (data?.data?.accessToken) {
       const user = (api.getState() as RootState).auth.user;
